e171a856b065ea127ce57e5ff8c79130ca055a92
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-12-03 21:53:49 +0530

    started with the frontend

---
+++ client/src/Components/Add_Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Add_Blogs = () => {
+  return (
+    <div className="mt-10 ml-[30rem]">
+        <input type="text" placeholder="write blog title" className="w-[40rem] h-[51px] border-4 border-black rounded-lg p-2
+        " />
+        <button className="h-[51px] p-2 ml-2 border-4 border-black rounded-lg font-semibold text-xl">Add</button>
+    </div>
+  )
+}
+
+export default Add_Blogs
---
+++ client/src/Components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Add_Blogs from './Add_Blogs'
+
+const Body = () => {
+  return (
+    <div className = "">
+        <h1 className="font-bold text-5xl mx-[40rem] mt-2">Blog App</h1>
+        <Add_Blogs />
+    </div>
+  )
+}
+
+export default Body
