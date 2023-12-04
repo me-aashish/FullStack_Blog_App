8d279bebf4829afb8617e9f4a9012f3584151619
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-12-04 22:39:45 +0530

    added 2 way state binding for blog title

--- client/src/Components/Add_Blogs.js
+++ client/src/Components/Add_Blogs.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add_Blogs = () => {
+  const [blogTitle, setBlogTitle] = useState(null);
+  const handleAddBlogTitle = () => {
+    console.log(blogTitle);
+  }
   return (
     <div className="mt-10 ml-[30rem]">
         <input type="text" placeholder="write blog title" className="w-[40rem] h-[51px] border-4 border-black rounded-lg p-2
-        " />
-        <button className="h-[51px] p-2 ml-2 border-4 border-black rounded-lg font-semibold text-xl">Add</button>
+        " onChange={e => setBlogTitle(e.target.value)} />
+        <button className="h-[51px] p-2 ml-2 border-4 border-black rounded-lg font-semibold text-xl" onClick={handleAddBlogTitle}>Add</button>
     </div>
   )
 }
