969cbdb6d3b2ac14df7e92f37705f6cab37098ab
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-12-06 00:31:42 +0530

    added cors

--- Server/Blogs/app.js
+++ Server/Blogs/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const { randomBytes } = require("crypto");
 
-
+app.use(cors());
 app.use(bodyParser.json());
+
 
 const posts = {}
 
