fd8517e601ee93f4df60ee7dce6b71e5aa870199
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-12-10 11:40:03 +0530

    added event datastore in event bus for event sync in case of any service failure

--- Server/EventBus/app.js
+++ Server/EventBus/app.js
@@ -5,8 +5,11 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = []
+
 app.post("/events", async(req,res) => {
     const  event  = req.body;
+    events.push(event);
     console.log(event);
      axios.post("http://localhost:4000/events", event);
      axios.post("http://localhost:4001/events", event);
@@ -14,6 +17,10 @@ app.post("/events", async(req,res) => {
      axios.post("http://localhost:4004/events", event);
 
     res.send({"status" : "OK"})
+})
+
+app.get("/events", (req,res) => {
+    res.send(events);
 })
 
 app.listen(4003, () => {
--- Server/QueryService/app.js
+++ Server/QueryService/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,42 +8,51 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const queryData = {}
+const queryData = {};
 
-app.post("/events", (req,res) => {
-    // console.log(req.body);
-    const { type } = req.body;
+const handleQuery = (type, data) => {
+  if (type === "BlogCreated") {
+    const { id, title } = data;
+    queryData[id] = {
+      id,
+      title,
+      comments: [],
+    };
+  } else if (type === "CommentCreated") {
+    const { blogId, id, content, status } = data;
+    queryData[blogId].comments.push({ id, content, status });
+  } else if (type == "CommentUpdated") {
+    const { blogId, id, content, newStatus } = data;
+    const blog = queryData[blogId];
+    const comment = blog.comments.find((comment) => comment.id === id);
 
-    if(type === "BlogCreated"){
-        const { data } = req.body;
-        const { id, title } = data
-        queryData[id] = {
-            id,
-            title,
-            comments: []
-        }
-    }else if(type === "CommentCreated"){
-        const { data } = req.body;
-        const { blogId, id, content, status } = data;
-        queryData[blogId].comments.push({id, content, status});
-    }
-    else if(type == "CommentUpdated"){
-        const { data } = req.body;
-        const { blogId, id, content, newStatus } = data;
-        const blog = queryData[blogId];
-        const comment = blog.comments.find(comment => comment.id === id)
+    comment.status = newStatus;
+    comment.content = content;
+  }
+};
 
-        comment.status = newStatus;
-        comment.content = content;
-    }
-    console.log(queryData);
-    res.send({});
-})
+app.post("/events", (req, res) => {
+  // console.log(req.body);
+  const { type, data } = req.body;
+  
+  handleQuery(type, data);
+    
+  res.send({});
+});
 
-app.get("/blogs", (req,res) => {
-    res.send(queryData);
-})
+app.get("/blogs", (req, res) => {
+  res.send(queryData);
+});
 
-app.listen(4002, () => {
-    console.log("Query service running on port 4002");
-})
+app.listen(4002, async() => {
+  console.log("Query service running on port 4002");
+  
+
+  const res = await axios.get("http://localhost:4003/events");
+
+  for(let event of res.data){
+    console.log("Processing event of type ", event.type);
+    handleQuery(event.type, event.data);
+  }
+
+});
